0f5231ba1de0c3c7b3eb46aade9eda76091efc95
jonathan-jipslok <jonathanpoli17@gmail.com>
2026-05-02 23:57:06 -0300

    feat: características do personagem adicionado

--- js/script.js
+++ js/script.js
@@ -35,6 +35,12 @@ async function pegarPaginaPersonagens(url){
 
 function inserirPersonagem(JsonPersonagem) {
 
+    let primeiroTextoP = `<p><b>Genero:</b> ${JsonPersonagem.Genero}</p><br>
+    <p><b>Estado:</b> ${JsonPersonagem.Estado}</p><br>
+    <p><b>Ocupacion:</b> ${JsonPersonagem.Ocupacion}</p><br>`
+    let segundoTextoP = JsonPersonagem.Historia
+    let contadorTexto = 1;
+
     // pega o elemento onde os cards serão inseridos
     let divCards = document.getElementById('divCards');
 
@@ -43,22 +49,39 @@ function inserirPersonagem(JsonPersonagem) {
     let h3Card = document.createElement('h3');
     let divCardImage = document.createElement('div');
     let pCard = document.createElement('p');
+    let btnCardHistoria = document.createElement('button')
 
-    // Adiciona as classes CSS aos elementos
+    // Adiciona as classes aos elementos
     divCard.classList.add('divCard');
     h3Card.classList.add('h3Card');
     divCardImage.classList.add('divCardImage');
     pCard.classList.add('pCard');
+    btnCardHistoria.classList.add("btnCardHistoria")
 
     // Adiciona o conteúdo aos elementos
     h3Card.textContent = JsonPersonagem.Nombre;
     divCardImage.style.backgroundImage = `url(${JsonPersonagem.Imagen})`;
-    pCard.textContent = JsonPersonagem.Historia;
+    pCard.innerHTML = primeiroTextoP
+    btnCardHistoria.textContent = "História"
+
+    //Evento do botão
+    btnCardHistoria.addEventListener('click', function() {
+        if(contadorTexto == 1) {
+            pCard.innerHTML = segundoTextoP
+            contadorTexto = 2
+            btnCardHistoria.innerText = "Características"
+        } else {
+            pCard.innerHTML = primeiroTextoP
+            contadorTexto = 1;
+            btnCardHistoria.innerText = "História"
+        }
+    })
 
     // Adiciona os Cards
     divCards.appendChild(divCard);
     divCard.appendChild(h3Card);
     divCard.appendChild(divCardImage);
+    divCard.appendChild(btnCardHistoria);
     divCard.appendChild(pCard);
 }
 
